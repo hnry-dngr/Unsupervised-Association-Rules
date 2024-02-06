778ee3aff6ff1a28c0a1c5efaa8a270a61cf62c3
hnry-dngr <nwokobiah@gmail.com>
2024-02-06 17:03:49 +0100

    Create Association Rule Minnig.py

---
+++ Association Rule Minnig.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 16:45:42 2024
+
+@author: HP
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import association_rules, apriori
+
+
+dataset = pd.read_csv("C:/Users/HP/Downloads/Market_Basket_Optimisation.csv", header = None)
+
+# ---> Data Cleaning and Transformation Phase
+# TASK 1: Create a list of lists
+Transactions = []
+
+for row in range(0, 7501):
+    each_transaction = []
+    for column in range(0, 20):
+        item = dataset.iloc[row, column]
+        if not isinstance(item, float):
+            each_transaction.append(item)
+    Transactions.append(each_transaction)
+
+# TASK 2: Create an ENCODED VERSION of your data
+encoder = TransactionEncoder()
+new_data = pd.DataFrame(encoder.fit_transform(Transactions), columns = encoder.columns_)
+
+# EVALUATION
+support = apriori(new_data, min_support = 0.03, use_colnames = True, low_memory = True)
+confidence = association_rules(support, metric = "confidence", min_threshold = 0.01)
+lift = association_rules(support, metric = "lift", min_threshold = 1.1)
